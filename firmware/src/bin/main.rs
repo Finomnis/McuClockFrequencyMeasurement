205572241be52e4cf15918f164b4d04b4ef8ad09
Finomnis <finomnis@gmail.com>
2022-01-21 14:55:03 +0100

    Add i2c initialization; minor refactoring

--- firmware/src/bin/main.rs
+++ firmware/src/bin/main.rs
@@ -6,20 +6,20 @@ use clock_frequency_measurement::hal;
 use hal::rcc::{Enable, Reset};
 use hal::stm32;
 
-fn configure_rel_timer(rel_timer: stm32::TIM14, rcc: &mut hal::rcc::Rcc) -> stm32::TIM14 {
+fn configure_core_timer(timer_core: stm32::TIM14, rcc: &mut hal::rcc::Rcc) -> stm32::TIM14 {
     stm32::TIM14::enable(rcc);
     stm32::TIM14::reset(rcc);
-    rel_timer.cr1.modify(|_, w| w.cen().clear_bit());
-    rel_timer.cnt.reset();
+    timer_core.cr1.modify(|_, w| w.cen().clear_bit());
+    timer_core.cnt.reset();
     let psc = 999; // Prescaler of 1000, exactly
     let arr = 0xffff; // Count to max
-    rel_timer.psc.write(|w| unsafe { w.psc().bits(psc) });
-    rel_timer.arr.write(|w| unsafe { w.bits(arr) });
-    rel_timer
+    timer_core.psc.write(|w| unsafe { w.psc().bits(psc) });
+    timer_core.arr.write(|w| unsafe { w.bits(arr) });
+    timer_core
         .cr1
         .modify(|_, w| w.cen().set_bit().urs().set_bit());
 
-    rel_timer
+    timer_core
 }
 
 #[rtic::app(device = clock_frequency_measurement::hal::stm32, peripherals = true)]
@@ -34,15 +34,15 @@ mod app {
 
     #[local]
     struct LocalResources {
-        rel_time_prev_actual: u16,
-        rel_time_prev_expected: u16,
+        core_time_prev_actual: u16,
+        core_time_prev_expected: u16,
     }
 
     #[shared]
     struct SharedResources {
         timer_1hz: Timer<stm32::TIM16>,
         led: PB0<Output<PushPull>>,
-        rel_timer: stm32::TIM14,
+        timer_core: stm32::TIM14,
     }
 
     #[init]
@@ -55,7 +55,10 @@ mod app {
         // Initialize GPIOs
         let mut rcc = ctx.device.RCC.constrain();
         let gpiob = ctx.device.GPIOB.split(&mut rcc);
+        let gpioa = ctx.device.GPIOA.split(&mut rcc);
         let led = gpiob.pb0.into_push_pull_output();
+        let i2c_sda = gpioa.pa10.into_open_drain_output();
+        let i2c_scl = gpioa.pa9.into_open_drain_output();
 
         // Initialize timers
         let mut timer_1hz = ctx.device.TIM16.timer(&mut rcc);
@@ -63,46 +66,49 @@ mod app {
         timer_1hz.listen();
 
         // Configure TIM14 to run with clock/1000
-        let rel_timer = super::configure_rel_timer(ctx.device.TIM14, &mut rcc);
+        let timer_core = super::configure_core_timer(ctx.device.TIM14, &mut rcc);
+
+        // Initialize I2C
+        let mut i2c =
+            ctx.device
+                .I2C1
+                .i2c(i2c_sda, i2c_scl, hal::i2c::Config::new(100.khz()), &mut rcc);
 
         (
             SharedResources {
+                timer_core,
                 timer_1hz,
                 led,
-                rel_timer,
             },
             LocalResources {
-                rel_time_prev_actual: 0,
-                rel_time_prev_expected: 0,
+                core_time_prev_actual: 0,
+                core_time_prev_expected: 0,
             },
             init::Monotonics(),
         )
     }
 
-    #[task(binds = TIM16, shared = [led, timer_1hz, rel_timer], local = [rel_time_prev_expected])]
+    #[task(binds = TIM16, shared = [led, timer_1hz, timer_core], local = [core_time_prev_expected])]
     fn measure_expected(mut ctx: measure_expected::Context) {
         ctx.shared.timer_1hz.lock(|timer| timer.clear_irq());
-
         ctx.shared.led.lock(|led| led.toggle().unwrap());
 
-        let rel_time = ctx
+        let core_time = ctx
             .shared
-            .rel_timer
+            .timer_core
             .lock(|timer| timer.cnt.read().cnt().bits());
 
-        let rel_time_diff = rel_time
-            .overflowing_sub(*ctx.local.rel_time_prev_expected)
-            .0;
-        *ctx.local.rel_time_prev_expected = rel_time;
+        let (core_time_diff, _) = core_time.overflowing_sub(*ctx.local.core_time_prev_expected);
+        *ctx.local.core_time_prev_expected = core_time;
 
         defmt::println!(
             "Expected: {}.{:03} MHz",
-            rel_time_diff / 1000,
-            rel_time_diff % 1000
+            core_time_diff / 1000,
+            core_time_diff % 1000
         );
     }
 
-    #[task(binds = TIM14, shared = [led, timer_1hz, rel_timer], local = [rel_time_prev_actual])]
+    #[task(binds = TIM14, shared = [led, timer_1hz, timer_core], local = [core_time_prev_actual])]
     fn measure_actual(mut ctx: measure_actual::Context) {
         // ctx.shared.timer_1hz.lock(|timer| timer.clear_irq());
 
